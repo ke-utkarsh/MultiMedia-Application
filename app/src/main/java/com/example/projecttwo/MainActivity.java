265f81f996bd2174f4234f303689a7466b61b6fd
Utkarsh Sharma <utkarsh51k@gmail.com>
2020-07-29 13:27:05 +0530

    After sigup and login ui

--- app/src/main/java/com/example/projecttwo/MainActivity.java
+++ app/src/main/java/com/example/projecttwo/MainActivity.java
@@ -16,6 +16,7 @@ import androidx.navigation.ui.NavigationUI;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,9 +26,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -44,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
--- app/src/main/java/com/example/projecttwo/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/projecttwo/ui/gallery/GalleryFragment.java
@@ -1,6 +1,8 @@
 package com.example.projecttwo.ui.gallery;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,8 @@ import android.arch.lifecycle.ViewModelProviders;
 import com.example.projecttwo.R;
 
 public class GalleryFragment extends Fragment {
+    TabLayout tabLayouts;
+    ViewPager viewPagers;
 
     private GalleryViewModel galleryViewModel;
 
@@ -22,6 +26,31 @@ public class GalleryFragment extends Fragment {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        tabLayouts=root.findViewById(R.id.tabLayoutS);
+        viewPagers=root.findViewById(R.id.viewPagerS);
+
+        tabLayouts.addTab(tabLayouts.newTab().setText("Login"));
+        tabLayouts.addTab(tabLayouts.newTab().setText("Signup"));
+        tabLayouts.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final Tabs2 adapter= new Tabs2(getActivity(),getChildFragmentManager(),tabLayouts.getTabCount());
+        viewPagers.setAdapter(adapter);
+
+        viewPagers.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayouts));
+        tabLayouts.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPagers.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
 
         return root;
     }
